e135b3fc6b653f97d659646c5beaa7855aada827
pchristopher1275 <pchristopher@gmail.com>
2018-10-09 20:56:08 -0700

    X

--- align.pl
+++ align.pl
@@ -3,26 +3,64 @@ use strict;
 use Carp;
 my $gSoxPath   = "./sox-14.4.2/sox";
 my $gVerbose   = 0;
-my $gInputDir  = "input";
+my $gTempTemplate = "/tmp/align.$$";
+my $gNextTempfile = 0;
+
+sub tempFile {
+    my $i = $gNextTempfile;
+    $gNextTempfile++;
+    return "$gNextTempfile.$i";
+}
 
 sub soxSilence {
     my ($inputFile, $outputFile) = @_;
 # ../../sox-14.4.2/sox TRACK17_18.WAV x.wav silence 1 0 0.000001
+
 }
 
 sub soxTrim {
-    my ($inputFile, $outputFile, $startSeconds, $lengthSeconds) = @_;
-# ../../sox-14.4.2/sox TRACK17_18.WAV x.wav trim start length
+   my ($inputFile, $outputFile, $startSeconds, $lengthSeconds) = @_;
+   run("$gSoxPath $inputFile $outputFile trim $startSeconds $lengthSeconds");
 }
 
-sub listProjects {
-
+sub soxStat {
+    my ($inputFile) = @_;
+    my $tfile = tempFile();
+    my @lines = backtick("$gSoxPath $inputFile $tfile stat");
+    chomp(@lines);
+    my %stat;
+    for my $line (@lines) {
+        if ($line =~ /Length/) {
+	    my (undef, $length) = split ":", $line;
+	    $length =~ s/\s//g;
+	    $stat{length} = $length;
+	}
+    }
+    die "Failed to find Length in $inputFile from soxStat" unless defined($stat{length});
+    return \%stat;
 }
 
 sub parseProjectName {
+    my ($projectName) = @_;
 
 }
 
+## listProjects returns all the project directories found in the input directory
+sub listProjects {
+}
+
+
+## listTracks picks up all the tracks in a project directory
 sub listTracks {
 
 }
+
+## findSilentTrimLength scans all of the tracks in a project directory and computes the start-point for the loops.
+## The start point is the longest length (considering all tracks and the master track) that captures ONLY silence 
+## for all the tracks.
+sub findSilentTrimLength {
+    my ($projectDir) = @_;
+    my $tfile = tempFile();
+     
+}
+
