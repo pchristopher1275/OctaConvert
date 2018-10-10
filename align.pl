0003df9e53f00a14694fad41b0c43e2f837f9d86
pchristopher1275 <pchristopher@gmail.com>
2018-10-09 20:36:50 -0700

    X

---
+++ align.pl
@@ -0,0 +1,28 @@
+#!/usr/bin/perl
+use strict;
+use Carp;
+my $gSoxPath   = "./sox-14.4.2/sox";
+my $gVerbose   = 0;
+my $gInputDir  = "input";
+
+sub soxSilence {
+    my ($inputFile, $outputFile) = @_;
+# ../../sox-14.4.2/sox TRACK17_18.WAV x.wav silence 1 0 0.000001
+}
+
+sub soxTrim {
+    my ($inputFile, $outputFile, $startSeconds, $lengthSeconds) = @_;
+# ../../sox-14.4.2/sox TRACK17_18.WAV x.wav trim start length
+}
+
+sub listProjects {
+
+}
+
+sub parseProjectName {
+
+}
+
+sub listTracks {
+
+}
